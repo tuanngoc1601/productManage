4622ee778bb1c7b107c02739dafada55a09d6fa3
btcs-manhdd <manhdd@btc-studio.io>
2023-01-08 12:39:08 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -10,28 +10,6 @@ import Error from "./page/Error";
 import SingleProduct from "./page/SingleProduct";
 import User from "./page/User";
 
-// Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD3qLbueLrzZdqV_DEqM15FGYr_KlqMuLE",
-  authDomain: "p01-product.firebaseapp.com",
-  projectId: "p01-product",
-  storageBucket: "p01-product.appspot.com",
-  messagingSenderId: "717743062706",
-  appId: "1:717743062706:web:9bdb1f7e82216d646753ab",
-  measurementId: "G-XZ803VF0J6",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 function App() {
   const { colors, sizes, categories } = useGetAllProductInfo();
   return (
